7fc94a98bd22ae673c05f9d8a3b5ada53df5a5ca
Becky <becky.chan@slalom.com>
2022-08-29 23:45:03 -0500

    update value

--- react-challenge/src/components/FlightSearch/index.js
+++ react-challenge/src/components/FlightSearch/index.js
@@ -4,7 +4,7 @@ import FlightSearchItem from './FlightSearchItem';
 import FlightSearchFooter from './FlightSearchFooter';
 import useFetchFlightResults from './useFetchFlightResults';
 import SortBy from './filters/SortBy';
-import { SortByDefaultOption } from './filters/SortBy/enums';
+import { SortByDefaultOption, SortByOptions } from './filters/SortBy/enums';
 
 export default function FlightSearch() {
   const [sortBy, setSortBy] = useState(SortByDefaultOption);
@@ -13,17 +13,45 @@ export default function FlightSearch() {
   const { flights } = useFetchFlightResults();
 
   // Only show 10 flight results per page
-  const paginatedFlights = flights.slice(0, 8);
+  // let paginatedFlights = flights.slice(0, 8);
+  const [paginatedFlights, setPaginatedFlights] = useState(flights.slice(0, 8))
+  console.log('paginatedFlights', paginatedFlights)
+
+  let handleOnChange = (event) => {
+    setSortBy(event.value)
+    console.log(`after sortby ${JSON.stringify(sortBy)}`)
+    console.log(`ON CHANGE ${event.value}`)
+
+    if (event.label === SortByOptions[0].label) {
+      let result = [...flights].sort((a, b) => a.score - b.score).slice(0,8)
+      setPaginatedFlights(result);
+    }
+    if (event.label === SortByOptions[1].label) {
+      let result = [...flights].sort((a, b) => a.price - b.price).slice(0,8);
+      setPaginatedFlights(result);
+    }
+    if (event.label === SortByOptions[2].label) {
+      let result  = [...flights].sort((a, b) => {
+        const firstFlight = new Date(a.departsAt);
+        const secondFlight = new Date(b.departsAt);
+        return firstFlight < secondFlight ? 1 : -1;
+      }
+      );
+      setPaginatedFlights(result);
+    }
+    console.log('page:', paginatedFlights)
+  }
 
   return (
     <div className="row">
       <div className="pane m-t-1">
+        <div>SORT BY ${JSON.stringify(sortBy)}</div>
         <div className="pane-header search-results__header">
           <div className="search-results__title">
             <b>Select an outbound flight</b>
             <p className="m-v-0 fade small">DEN → CHI</p>
           </div>
-          <SortBy value={sortBy} onChange={setSortBy} />
+          <SortBy value={sortBy.value} onChange={(e) => handleOnChange(e)} />
         </div>
         {/* Display Flight Results */}
         <div className="pane-content">
